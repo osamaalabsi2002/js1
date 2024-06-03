d6bc924bcc24d6ec8cf7b138a7a38ef2ce3f9e1e
osamaalabsi2002 <alabsiosama33@gmail.com>
2024-06-03 16:15:12 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,132 @@
+/*
+let username = prompt("Enter username: ");
+let password = prompt("Enter password: ");
+
+const correctUsername = "osama"; 
+const correctPassword = "123"; 
+
+if (username === correctUsername && password === correctPassword) {
+  console.log("Access granted");
+} else {
+  console.log("Access denied");
+}
+
+
+
+
+
+
+let num1 =1;
+let num2 =15;
+if(num1 > 10 && num2 > 10)
+    {
+
+        console.log("Both numbers are greater than 10")
+    }
+
+    else {
+        console.log("At least one number is not greater than 10");
+      }
+
+
+      let number =105;
+if (number >= 10 && number <= 20)
+
+
+    {
+
+        console.log("The number is between 10 and 20")
+    }
+
+    else {
+        console.log("The number is not between 10 and 20");
+      }
+      
+
+
+     
+      let age = prompt("Enter age: ");
+      
+    
+      
+      if (age >=18 ) {
+        console.log("y");
+      } else {
+        console.log("n");
+      }
+
+
+
+ let number= prompt("enter num")
+ if( number>10 && number<0){
+    console.log("this nummber greter than 10")
+
+ }
+ else{
+
+    console.log("this nummber is not greter than 10")
+
+ }
+
+ let Number =prompt("enter num");
+ if (Number >= 5 && Number <= 10  || Number >= 20 && Number <= 25 )
+ 
+ 
+     {
+ 
+         console.log("The number is valid")
+     }
+ 
+     else {
+         console.log("The number is not valid");
+       }
+        
+       let  password = prompt("enter password")
+       if( password == "password" || password == "secret")
+        {
+            console.log("Access granted")
+            
+        }
+        else{ 
+            console.log("is not Access granted")
+        }
+
+let number=prompt("enter number")
+if (number >= 0 && number <=100 && number%2==0)
+    {
+        console.log(" the number is even")
+    }
+    else{
+       console.log ("the number is odd")
+    
+    }
+
+
+    let num1 = prompt("enter num")
+    let num2 =- prompt("enter num 2")
+     
+    if (num1 <0 || num2<0){
+        console.log("Both numbers are positive")
+    }
+    else{
+        console.log("at least one numbe is negative")
+    }
+        
+    let age=prompt ("enter age")
+    let  isstudent =true
+    if (age<18 || isstudent ){
+        console.log("You qualify for a discount")
+    }
+    else{
+        console.log("You do not qualify for a discount")
+    }
+
+    */
+    
+  
+
+
+
+       
+
+       
